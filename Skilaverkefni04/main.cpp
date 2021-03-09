d8fcad22e8004da396ade1c8855b272af42dcd0e
elmarolafsson <elmarolafsson@hotmail.com>
2021-03-09 15:45:49 +0000

    shaket

--- Skilaverkefni04/main.cpp
+++ Skilaverkefni04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Being {
@@ -16,6 +17,7 @@ public:
         this->strength = strength;
         this->intelligence = intelligence;
     };
+    virtual void print_information() = 0;
 protected:
     int health;
     int strength;
@@ -32,10 +34,13 @@ public:
         this->gender = gender;
         this->fear = fear;
     }
-    friend ostream &operator<<(ostream &out, const Person &org){
-        out << "health: " << org.health << " strength: " << org.strength << " Intelligence " << org.intelligence 
-            << " gender: " << org.gender << " fear: " << org.fear;
-        return out;
+    virtual void print_information(){
+        cout << "Person" << endl;
+        cout << "Health: " << health << endl;
+        cout << "Strength: " << strength << endl;
+        cout << "Intelligence: " << intelligence << endl;
+        cout << "Gender: " << gender << endl;
+        cout << "Fear: " << fear << endl;
     }
 protected:
     string gender;
@@ -50,10 +55,9 @@ public:
         : Person(health, strength, intelligence, gender, fear){
             this->terror = terror;
     }
-    friend ostream &operator<<(ostream &out, const Investigator &org){
-        out << "health: " << org.health << " strength: " << org.strength << " Intelligence " << org.intelligence 
-            << " gender: " << org.gender << " fear: " << org.fear << " terror " << org.terror;
-        return out;
+    virtual void print_information(){
+        Person::print_information();
+        cout << "Terror: " << terror << endl;
     }
 private:
     int terror;
@@ -69,10 +73,13 @@ public:
         this->natural = natural;
         this->disquiet = disquiet;
     }
-    friend ostream &operator<<(ostream &out, const Creature &org){
-        out << "health: " << org.health << " strength: " << org.strength << " Intelligence " << org.intelligence 
-            << " natural: " << org.natural << " disquiet: " << org.disquiet;
-        return out;
+    virtual void print_information(){
+        cout << "Creature" << endl;
+        cout << "Health: " << health << endl;
+        cout << "Strength: " << strength << endl;
+        cout << "Intelligence: " << intelligence << endl;
+        cout << "Natural: " << natural << endl;
+        cout << "Disquiet: " << disquiet << endl;
     }
 protected:
     bool natural;
@@ -86,15 +93,94 @@ public:
     Eldritch(int health, int strength, int intelligence, int traumatism) : Creature(health, strength, intelligence, 1, 10){
         this->traumatism = traumatism;
     }
+    virtual void print_information(){
+        Creature::print_information();
+        cout << "Traumatism: " << traumatism << endl;
+    }
 private:
     int traumatism;
 };
 
+void show_menu();
+
 int main() {
-    Person person(5, 2, 1, "male", 10);
-    Creature creature(8,3,4,1,8);
-    Investigator inv(4,6,2,"female",5,1);
-    cout << inv << endl;
-    cout << creature << endl;
-    cout << person << endl;
+    vector<Being *> beings;
+    int selection;
+    int choice = 0;
+    bool quit = false;
+    enum Main {
+        CREATE = 1,
+        DISPLAY = 2,
+        QUIT = 3
+    };
+    enum Choice {
+        PERSON = 1, 
+        INVESTIGATOR = 2, 
+        CREATURE = 3,
+        ELDRITCH = 4,
+        BACK = 5
+    };
+    string name;
+    string gender;
+    while (!quit)
+    {
+        show_menu();
+        cin >> selection;
+        switch (selection)
+        {
+        case CREATE:
+            cout << "1. Person" << endl;
+            cout << "2. Investigator" << endl;
+            cout << "3. Creature" << endl;
+            cout << "4. Eldritch horror" << endl;
+            cout << "5. Go back" << endl;
+            cin >> choice;
+            switch (choice)
+            {
+            case PERSON:
+                cout << "Creating a person" << endl;
+                
+                cout << "Enter Name: ";
+                cin >> name;
+                cout << "Enter Gender: ";
+                cin >> gender;
+                cout << name << " " << gender << endl;
+                break;
+            case CREATURE:
+                cout << "creating a creature" << endl;
+                break;
+            case INVESTIGATOR:
+                cout << "creating an investigator" << endl;
+                break;
+            case ELDRITCH:
+                cout << "creating an eldritch horror" << endl;
+                break;
+
+            default:
+                break;
+            }
+            
+            
+        
+        default:
+            break;
+        }
+
+    }
+    
+    beings.push_back(new Person(4, 3, 2, "female", 10));
+    beings.push_back(new Eldritch(8, 4, 6, 9));
+    beings.push_back(new Investigator(5, 7, 4, "male", 7, 2));
+    beings.push_back(new Creature(5, 9, 4, 1, 10));
+
+    for (Being *b : beings){
+        b->print_information();
+        cout << endl;
+    }
+}
+
+void show_menu(){
+    cout << "1. Create new character" << endl;
+    cout << "2. Display characters"<< endl;
+    cout << "3. Quit" << endl;
 }
