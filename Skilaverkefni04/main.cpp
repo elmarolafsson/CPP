ff11acbe26263a72ac1630df2366fbfe54f8b444
Arnar <arnaro17@ru.is>
2021-03-09 14:13:40 +0000

    categories are here UwU

--- Skilaverkefni04/main.cpp
+++ Skilaverkefni04/main.cpp
@@ -37,13 +37,64 @@ public:
             << " gender: " << org.gender << " fear: " << org.fear;
         return out;
     }
-private:
+protected:
     string gender;
     int fear;
 };
+class Investigator : public Person{
+public:
+    Investigator(): Person(){
 
+    }
+    Investigator(int health, int strength, int intelligence, string gender, int fear, int terror) 
+        : Person(health, strength, intelligence, gender, fear){
+            this->terror = terror;
+    }
+    friend ostream &operator<<(ostream &out, const Investigator &org){
+        out << "health: " << org.health << " strength: " << org.strength << " Intelligence " << org.intelligence 
+            << " gender: " << org.gender << " fear: " << org.fear << " terror " << org.terror;
+        return out;
+    }
+private:
+    int terror;
+};
+
+class Creature : public Being {
+public:
+
+    Creature() : Being(){
+        
+    }
+    Creature(int health, int strength, int intelligence, bool natural, int disquiet) : Being(health,strength,intelligence){
+        this->natural = natural;
+        this->disquiet = disquiet;
+    }
+    friend ostream &operator<<(ostream &out, const Creature &org){
+        out << "health: " << org.health << " strength: " << org.strength << " Intelligence " << org.intelligence 
+            << " natural: " << org.natural << " disquiet: " << org.disquiet;
+        return out;
+    }
+protected:
+    bool natural;
+    int disquiet;
+};
+class Eldritch : public Creature{
+public: 
+    Eldritch() : Creature(){
+
+    }
+    Eldritch(int health, int strength, int intelligence, int traumatism) : Creature(health, strength, intelligence, 1, 10){
+        this->traumatism = traumatism;
+    }
+private:
+    int traumatism;
+};
 
 int main() {
     Person person(5, 2, 1, "male", 10);
+    Creature creature(8,3,4,1,8);
+    Investigator inv(4,6,2,"female",5,1);
+    cout << inv << endl;
+    cout << creature << endl;
     cout << person << endl;
 }
